3a322eca47221f8e02a58222a35136538bbee747
Davies Ashley <ashdavies@users.noreply.github.com>
2022-08-29 23:17:18 +0200

    Add molecule view model example

--- app/src/main/java/io/ashdavies/sample/LoginViewModel.kt
+++ app/src/main/java/io/ashdavies/sample/LoginViewModel.kt
@@ -1,12 +1,16 @@
 package io.ashdavies.sample
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.produceState
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
+import app.cash.molecule.RecompositionClock
+import app.cash.molecule.launchMolecule
 import kotlinx.coroutines.launch
 
 /**
@@ -21,13 +25,23 @@ class LoginViewModel(private val service: SessionService) : ViewModel() {
     viewModelScope.launch {
       viewState = service.login(
         username = username,
-        password = password
+        password = password,
       )
     }
   }
 }
 
+class MoleculeViewModel(private val service: SessionService) : ViewModel() {
+  val viewState = viewModelScope.launchMolecule(RecompositionClock.ContextClock) {
+    val serviceResult by produceState<LoginResult?>(null) {
+      value = service.login(TODO(/* username */), TODO(/* password */))
+    }
+    serviceResult
+  }
+}
+
 @Composable
-fun LoginScreen(viewModel: LoginViewModel = viewModel()) {
-  val viewState: LoginResult? = viewModel.viewState
+fun LoginScreen(viewModel: MoleculeViewModel = viewModel()) {
+  val viewState: LoginResult? by viewModel.viewState.collectAsState()
+  /* ... */
 }
