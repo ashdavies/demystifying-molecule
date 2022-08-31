73ae2633b877fbb2fb3fdbdbc290b7562c6f5190
Bill Phillips <wphillips@squareup.com>
2022-08-31 13:01:15 -0700

    Add ContextClock example

--- app/src/test/java/io/ashdavies/sample/LoginPresenterTest.kt
+++ app/src/test/java/io/ashdavies/sample/LoginPresenterTest.kt
@@ -1,5 +1,7 @@
 package io.ashdavies.sample
 
+import androidx.compose.runtime.BroadcastFrameClock
+import androidx.compose.runtime.MonotonicFrameClock
 import app.cash.molecule.RecompositionClock
 import app.cash.molecule.moleculeFlow
 import app.cash.turbine.test
@@ -9,11 +11,44 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import kotlinx.coroutines.yield
 import org.junit.Test
 import kotlin.test.assertEquals
 
 internal class LoginPresenterTest {
+  @Test
+  fun withContextClock() = runBlocking {
+    val goTos = Channel<Screen>(UNLIMITED)
+    val sessionService = FakeSessionService()
+    val events = MutableSharedFlow<LoginUiEvent>()
+    val username = "username"
+    val password = "password"
+    val clock = BroadcastFrameClock()
+
+    val presenter = LoginPresenter(sessionService, goTos::trySend)
+    withContext(clock) {
+      moleculeFlow(RecompositionClock.ContextClock) {
+        presenter.UiModel(events)
+      }.test {
+        // Fire up initial LaunchedEffects (if any)
+        yield()
+        assertEquals(LoginUiModel.Content, awaitItem())
+        events.emit(LoginUiEvent.Submit(username, password))
+        // push event into composition, wait for recomposer to request
+        // a new frame
+        yield(); yield();
+        clock.sendFrame(0)
+
+        assertEquals(LoginUiModel.Loading, awaitItem())
+        assertEquals(LoginAttempt(username, password), sessionService.loginAttempts.awaitValue())
+        sessionService.loginResults.trySend(LoginResult.Success)
+
+        assertEquals(LoggedInScreen(username), goTos.awaitValue())
+      }
+    }
+  }
+
   @Test
   fun works() = runBlocking {
     val goTos = Channel<Screen>(UNLIMITED)
